51f807f66bce885819ed380a4086d6a1dd62c9e8
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2023-09-03 12:58:56 -0700

    Update main.cpp header documentation

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 //******************************************************************************
 // File name:   main.cpp
-// Author:      
-// Date:        
-// Class:       
-// Assignment:  
-// Purpose:    
-// Hours: 
+// Author:      CS Faculty
+// Date:        8/22/2023
+// Class:       CS150-xx (xx is your section number)
+// Assignment:  Hello World
+// Purpose:     Display the message Hello World to the screen
+// Hours:       0.25
 //******************************************************************************
 
 #include <iostream>
